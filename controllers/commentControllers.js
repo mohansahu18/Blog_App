269eaf8f75c0d0e8daa8eb3aec55634996610a98
mohan lal sahu <mohansahu18111811@gmail.com>
2023-07-05 23:37:12 +0530

    completed createComment controller

---
+++ controllers/commentControllers.js
@@ -0,0 +1,38 @@
+// import the model
+const comments = require("../models/commentModels")
+const post = require("../models/postModels")
+
+// logic
+const createComment = async (req, res) => {
+    try {
+        // fetch data from request body
+        const { post, user, body } = req.body
+        // create the comment object
+        const comment = new Comment({
+            post, user, body
+        })
+        // saved the comment into database
+        const savedComment = await comment.Save()
+        // find the post by id ,add the new comments to its comments array
+        const updatePost = await post.findByIdAndUpdate(
+            post, { $push: { comments: savedComment._id } }, { new: true })
+            .populate("comments")//populate the comment array with comment documents
+            .exec()
+        return res.status(200).json({
+            success: true,
+            data: updatePost,
+            message: "New comment created successfully"
+        })
+    }
+    catch (err) {
+        console.log(err);
+        console.error(err);
+        return res.status(500).json({
+            message: "Internal Server Error",
+        })
+    }
+
+}
+
+// export
+module.exports = { createComment }
